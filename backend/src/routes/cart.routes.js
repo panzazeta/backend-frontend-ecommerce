0620917e225733f72f996d23afc444c053871402
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-11-03 14:09:31 -0300

    add mw user cart

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getCart, postCart, deleteCart, deleteCartProduct, postCartProduct, putCart, putCartProduct } from "../controllers/cart.controller.js";
+import { passportError, authorization } from "../utils/messagesError.js";
 
 const cartRouter = Router()
 
 cartRouter.get('/:id', getCart);
 cartRouter.post('/', postCart);
-cartRouter.post('/:cid/products/:pid', postCartProduct)
+cartRouter.post('/:cid/products/:pid',passportError('jwt'), authorization('user'), postCartProduct) //solo user puede agregar productos al cart
 cartRouter.delete('/:cid/products/:pid', deleteCartProduct);
 cartRouter.put('/:cid', putCart);
 cartRouter.put('/:cid/products/:pid', putCartProduct);
