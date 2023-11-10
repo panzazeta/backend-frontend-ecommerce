6c8a394dde48e78459158cc25a3ba564e6c466ca
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-11-10 13:24:26 -0300

    cart purchase nofun

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -3,9 +3,9 @@ import { productModel } from "../models/products.models.js";
 import { ticketModel } from "../models/tickets.models.js"
 
 export const getCart = async (req, res) => {
-    const { id } = req.params
+    const { cid } = req.params
     try {
-        const cart = await cartModel.findById(id)
+        const cart = await cartModel.findById(cid)
         if (cart)
             res.status(200).send({ respuesta: 'OK', mensaje: cart })
         else
@@ -155,50 +155,50 @@ export const deleteCart = async (req, res) => {
     }
 };
 
-// export const postCheckout = async (req, res) => {
-//     const cartId = req.params.cid;
+export const postCheckout = async (req, res) => {
+    const cartId = req.params.cid;
 
-//   try {
-//     const cart = await cartModel.findById(cartId).populate("items.product");
-//     if (!cart) {
-//       return res.status(404).json({ message: "Carrito no encontrado" });
-//     }
+  try {
+    const cart = await cartModel.findById(cartId).populate("items.product");
+    if (!cart) {
+      return res.status(404).json({ message: "Carrito no encontrado" });
+    }
 
-//     const productsNotProcessed = []; 
+    const productsNotProcessed = []; 
 
-//     for (const item of cart.items) {
-//       const product = item.product;
-//       const requestedQuantity = item.quantity;
+    for (const item of cart.items) {
+      const product = item.product;
+      const requestedQuantity = item.quantity;
 
-//       if (product.stock >= requestedQuantity) {
-//         // El producto tiene suficiente stock, restarlo
-//         product.stock -= requestedQuantity;
-//         await product.save();
-//       } else {
-//         // si el producto no tiene suficiente stock se almacenan en los no procesados
-//         productsNotProcessed.push(product._id);
-//       }
-//     }
+      if (product.stock >= requestedQuantity) {
+        // El producto tiene suficiente stock, restarlo
+        product.stock -= requestedQuantity;
+        await product.save();
+      } else {
+        // si el producto no tiene suficiente stock se almacenan en los no procesados
+        productsNotProcessed.push(product._id);
+      }
+    }
 
-//     // se actualiza el carrito con los productos no procesados
-//     cart.items = cart.items.filter(
-//       (cartItem) => !productsNotProcessed.includes(cartItem.product._id)
-//     );
-//     await cart.save();
+    // se actualiza el carrito con los productos no procesados
+    cart.items = cart.items.filter(
+      (cartItem) => !productsNotProcessed.includes(cartItem.product._id)
+    );
+    await cart.save();
 
-//     // se crea un ticket con los datos de la compra
-//     const ticket = new ticketModel({
-//       amount: cart.total, // Supongo que el carrito tiene un campo total
-//       purchaser: cart.userEmail, // O donde se almacena el correo del usuario
-//     });
-//     await ticket.save();
+    // se crea un ticket con los datos de la compra
+    const ticket = new ticketModel({
+      amount: cart.total, // Supongo que el carrito tiene un campo total
+      purchaser: cart.userEmail, // O donde se almacena el correo del usuario
+    });
+    await ticket.save();
 
-//     return res.status(200).json({
-//       message: "Compra finalizada exitosamente",
-//       productsNotProcessed: productsNotProcessed,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ message: "Error al procesar la compra" });
-//   }
-// };
+    return res.status(200).json({
+      message: "Compra finalizada exitosamente",
+      productsNotProcessed: productsNotProcessed,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error al procesar la compra" });
+  }
+};
--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import { getCart, postCart, deleteCart, deleteCartProduct, postCartProduct, putCart, putCartProduct } from "../controllers/cart.controller.js";
+import { getCart, postCart, deleteCart, deleteCartProduct, postCartProduct, putCart, putCartProduct, postCheckout } from "../controllers/cart.controller.js";
 import { passportError, authorization } from "../utils/messagesError.js";
 
 const cartRouter = Router()
 
-cartRouter.get('/:id', getCart);
+cartRouter.get('/:cid', getCart);
 cartRouter.post('/', postCart);
 cartRouter.post('/:cid/products/:pid', passportError('jwt'), authorization('user'), postCartProduct) //solo user puede agregar productos al cart
 cartRouter.delete('/:cid/products/:pid', deleteCartProduct);
 cartRouter.put('/:cid', putCart);
 cartRouter.put('/:cid/products/:pid', putCartProduct);
 cartRouter.delete('/:cid', deleteCart);
-// cartRouter.get('/:cid/purchase', postCheckout);
+cartRouter.get('/:cid/purchase', postCheckout);
 
 export default cartRouter
