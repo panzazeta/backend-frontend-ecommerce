86c9364e2fc583906104708e42fd59553a4ea2e6
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-11-11 12:00:56 -0300

    add code

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -165,64 +165,45 @@ export const postCheckout = async (req, res) => {
             return res.status(404).send({ respuesta: 'Error en finalizar compra', mensaje: 'Cart Not Found' });
         }
 
-        const productsNotPurchased = []; // Almacenar los productos que no pudieron comprarse
-        let totalAmount = 0; // Inicializar el total a 0
+        const productosNoComprados = []; 
+        let totalAmount = 0; 
+        const Subtotales = {}; 
 
-        const categorySubtotals = {}; // Almacenar sumas parciales por categoría
-
-        // Iterar sobre los productos en el carrito
         for (const product of cart.products) {
             const { id_prod, quantity } = product;
 
-            // Obtener el producto de la base de datos
-            const dbProduct = await productModel.findById(id_prod);
+            const dbProduct = await productModel.findById(id_prod); //busco el producto en mongodb
 
             if (!dbProduct) {
                 return res.status(404).send({ respuesta: 'Error en finalizar compra', mensaje: 'Product Not Found' });
             }
 
-            // Verificar si hay suficiente stock
             if (dbProduct.stock >= quantity) {
-                // Restar la cantidad del carrito al stock del producto
                 dbProduct.stock -= quantity;
-                
-                // Actualizar el producto en la base de datos
                 await dbProduct.save();
+                const prodSubtotal = dbProduct.price * quantity;
 
-                // Calcular el subtotal para este producto
-                const subtotal = dbProduct.price * quantity;
-
-                // Acumular el subtotal en la categoría correspondiente
-                if (!categorySubtotals[dbProduct.category]) {
-                    categorySubtotals[dbProduct.category] = 0;
+            if (!Subtotales[dbProduct.category]) {
+                Subtotales[dbProduct.category] = 0;
                 }
 
-                categorySubtotals[dbProduct.category] += subtotal;
+                Subtotales[dbProduct.category] += prodSubtotal;
+                totalAmount += prodSubtotal;
 
-                // Acumular el subtotal en el total general
-                totalAmount += subtotal;
             } else {
-                // Si no hay suficiente stock, agregar el producto a la lista de no comprados
-                productsNotPurchased.push(id_prod);
-
-                // Opcional: Eliminar el producto del carrito
+                productosNoComprados.push(id_prod);
                 cart.products = cart.products.filter(item => item.id_prod.toString() !== id_prod);
             }
         }
 
-        // Crear un ticket con la información de la compra y el total calculado
+        // TICKET CHECKOUT
         const ticket = await ticketModel.create({ amount: totalAmount, cart });
 
-        // Si hay productos que no pudieron comprarse, actualizar el carrito del usuario
         if (productsNotPurchased.length > 0) {
-            // Filtrar los productos que no se pudieron comprar
             cart.products = cart.products.filter(item => productsNotPurchased.includes(item.id_prod.toString()));
-
-            // Actualizar el carrito en la base de datos
             await cartModel.findByIdAndUpdate(cid, { products: cart.products });
         } else {
-            // Si todos los productos se compraron, vaciar el carrito
-            await cartModel.findByIdAndUpdate(cid, { products: [] });
+            await cartModel.findByIdAndUpdate(cid, { products: [] }); //vacio cart luego del purchase
         }
 
         res.status(200).send({ respuesta: 'OK', mensaje: 'Compra realizada con éxito', ticket });
