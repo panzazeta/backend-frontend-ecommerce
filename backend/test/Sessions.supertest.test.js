b77f824c61e1cb58b7fe7b67eff0f631353f271b
panzazeta <76927307+panzazeta@users.noreply.github.com>
2024-01-03 12:43:00 -0300

    session ok

--- backend/test/Sessions.supertest.test.js
+++ backend/test/Sessions.supertest.test.js
@@ -11,22 +11,22 @@ describe('Test Users Session api/sessions', function () {
     let cookie = {};
     this.timeout(5000);
 
-    // it("Ruta: api/sessions/register con metodo POST", async () => {
-    //     const newUser = {
-    //         first_name: "Gogol",
-    //         last_name: "Perezori",
-    //         age: 26,
-    //         email: "gogo@gmail.com",
-    //         password: "sarassa"
-    //     }
+    it("Ruta: api/sessions/register con metodo POST", async () => {
+        const newUser = {
+            first_name: "Gogol",
+            last_name: "Perezori",
+            age: 26,
+            email: "gogo@gmail.com",
+            password: "sarassa"
+        }
 
-    //     const { statusCode, _body } = await requester
-    //     .post("/api/sessions/register")
-    //     .send(newUser);
+        const { statusCode, _body } = await requester
+        .post("/api/sessions/register")
+        .send(newUser);
   
-    //   expect(statusCode).to.equal(201);
-    //   expect(_body.mensaje).to.equal("Usuario registrado");
-    // });
+      expect(statusCode).to.equal(201);
+      expect(_body.mensaje).to.equal("Usuario registrado");
+    });
        
     it("Ruta: api/sessions/login con metodo POST", async () => {
         const user = {
@@ -57,8 +57,6 @@ describe('Test Users Session api/sessions', function () {
         const { _body } = await requester
         .get('/api/sessions/current')
         .set('Cookie', [`${cookie.name}=${cookie.value}`])
-
-        console.log(_body.email)
     
         expect(_body.email).to.be.equal('gogo@gmail.com');
     });
