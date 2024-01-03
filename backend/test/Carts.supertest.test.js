11875e1c519111c3e9ba80213ce8bc8a9be30cf1
panzazeta <76927307+panzazeta@users.noreply.github.com>
2024-01-03 13:47:46 -0300

    cart test falla

--- backend/test/Carts.supertest.test.js
+++ backend/test/Carts.supertest.test.js
@@ -1,31 +1,44 @@
-import mongoose from "mongoose";
 import "dotenv/config";
 import {expect} from "chai";
-import supertest from "supertest";
+import mongoose from 'mongoose'
+import supertest from 'supertest'
 
-const requester = supertest("http://localhost:4000");
+const requester = supertest("http://localhost:4000/api");
 
 await mongoose.connect(process.env.MONGO_URL);
+let cookie = {};
 
-describe("Test CRUD del carrito en api/carts", function () {
-  it("Ruta: api/carts metodo Post", async () => {
-    const { ok } = await requester.post("/api/carts");
+describe("Test de carritos en la ruta api/carts", function () {
+    this.timeout(5000);
 
+  it("Ruta: api/sessions/login con metodo POST", async function () {
+        const user = {
+            email: "jorge13@gmail.com",
+            password: "lucas1"
+        }
+
+    const { _body, ok } = await requester.post("/sessions/login").send(user);
+    cookie = _body.cookie;
     expect(ok).to.be.ok;
   });
-});
 
 describe("Pruebas para la ruta /api/carts/:cid/products/:pid", () => {
   it("Ruta: api/carts metodo Post", async () => {
     
-    const carritoId = "6593ecd5c8a4d31e8816e417";
-    const productoId = "65022390bf2ac75bb917837c";
-    const cantidad = 34;
+    const cartId = "65036104a64cefd4eca177a1";
+    const productId = "650223e3bf2ac75bb9178382";
+    const cantidad = 2;
 
     const response = await requester
-      .post(`/api/carts/${carritoId}/products/${productoId}`)
+      .post(`/api/carts/${cartId}/products/${productId}`)
       .send({ quantity: cantidad });
 
     expect(response.status).to.equal(200);
   });
+})
+
+after(async () => {
+  await mongoose.disconnect();
+});
+
 });
