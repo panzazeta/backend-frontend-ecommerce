c8cf7af4c1d0de3dc1b08fe53bfce69493e6ce43
panzazeta <76927307+panzazeta@users.noreply.github.com>
2024-01-03 09:03:37 -0300

    supertest session 2 ok

--- backend/test/Sessions.supertest.test.js
+++ backend/test/Sessions.supertest.test.js
@@ -11,42 +11,46 @@ describe('Test Users Session api/sessions', function () {
     let cookie = {};
     this.timeout(5000);
 
-    it("Ruta: api/sessions/register con metodo POST", async () => {
-        const newUser = {
-            first_name: "Gogol",
-            last_name: "Perezori",
-            age: 26,
-            email: "gogo@gmail.com",
-            password: "sarassa"
-        }
+    // it("Ruta: api/sessions/register con metodo POST", async () => {
+    //     const newUser = {
+    //         first_name: "Gogol",
+    //         last_name: "Perezori",
+    //         age: 26,
+    //         email: "gogo@gmail.com",
+    //         password: "sarassa"
+    //     }
 
-        const { statusCode, _body } = await requester
-        .post("/api/sessions/register")
-        .send(newUser);
+    //     const { statusCode, _body } = await requester
+    //     .post("/api/sessions/register")
+    //     .send(newUser);
   
-      expect(statusCode).to.equal(201);
-      expect(_body.mensaje).to.equal("Usuario registrado");
-    });
+    //   expect(statusCode).to.equal(201);
+    //   expect(_body.mensaje).to.equal("Usuario registrado");
+    // });
        
     // it("Ruta: api/sessions/login con metodo POST", async () => {
     //     const user = {
-    //         email: "san@san.com",
-    //         password: "sa@sa.com"
+    //         email: "gogo@gmail.com",
+    //         password: "sarassa"
     //     }
 
-    //     const resultado = await requester.post('/api/sessions/login').send(user)
-    //     const cookieResult = resultado.headers['set-cookie'][0]
+    //     const response = await requester.post('/api/sessions/login').send(user);
 
-    //     expect(cookieResult).to.be.ok
+    //     expect(response.headers["set-cookie"]).to.be.an("array").that.is.not.empty;
 
+    //     const cookieResult = response.headers["set-cookie"][0];
+    
+    //     const [cookieName, cookieValue] = cookieResult.split("=");
+    
     //     cookie = {
-    //         name: cookieResult.split("=")[0],
-    //         value: cookieResult.split("=")[1]
-    //     }
-
-    //     expect(cookie.name).to.be.ok.and.equal('coderCookie')
-    //     expect(cookie.value).to.be.ok
-    // })
+    //       name: cookieName,
+    //       value: cookieValue,
+    //     };
+    
+    //     expect(cookieName).to.equal("connect.sid"); //qué onda connect.sid?
+    //     expect(cookieValue).to.be.ok;
+    //   });
+   
 
     // it("Ruta: api/session/current con metodo GET", async () => {
 
