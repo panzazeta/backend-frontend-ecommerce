2b3930b8404c60afa3558a3db8bc60b59ab7c22c
panzazeta <76927307+panzazeta@users.noreply.github.com>
2024-01-05 08:26:26 -0300

    test cart ok

--- backend/test/Carts.supertest.test.js
+++ backend/test/Carts.supertest.test.js
@@ -6,41 +6,18 @@ import supertest from 'supertest'
 const requester = supertest("http://localhost:4000/api");
 
 await mongoose.connect(process.env.MONGO_URL);
-let cookie = {};
 
 describe("Test de carritos en la ruta api/carts", function () {
     this.timeout(5000);
 
-  it("Ruta: api/sessions/login con metodo POST", async function () {
-        const user = {
-            email: "gogo@gmail.com",
-            password: "sarassa"
-        }
-
-    const { _body, ok } = await requester.post("/sessions/login").send(user);
-    cookie = _body.cookie;
-    expect(ok).to.be.ok;
-  });
-
-describe("Pruebas para la ruta /api/carts/:cid/products/:pid", () => {
-  it("Ruta: api/carts metodo Post", async () => {
+  it("Ruta /api/carts/:cid con método DELETE", async function () {
     
-    const cartId = "65036104a64cefd4eca177a1";
-    const productId = "650223e3bf2ac75bb9178382";
-    const cantidad = {
-      quantity: 5,
-    };
+    const cid = "65954973ee0b725ba1ff65cb";
 
     const response = await requester
-      .post(`/api/carts/${cartId}/products/${productId}`)
-      .send(cantidad);
-    
-    console.log("Mensaje!");
-    console.log(response.body);
-    
-    expect(response.status).to.equal(200);
+    .delete(`/carts/${cid}`)
+    expect(response).to.be.ok;
   });
-})
 
 after(async () => {
   await mongoose.disconnect();
